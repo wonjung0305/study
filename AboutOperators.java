af279670837e2fe333b4c648043604f884297fc7
wonjung0305 <wonjung1919@gmail.com>
2025-04-13 21:50:51 +0900

    4월 13일 3주차 공부

---
+++ AboutOperators.java
@@ -0,0 +1,25 @@
+public class AboutOperators {
+  public static void main(String[] args) {
+    // 산술 연산
+    // + 더하기 a + b
+    // - 빼기 a - b
+    // * 곱하기 a * b
+    // / 나누기 a / b
+    // % 나머지 a % b
+      // 정수 나눗셈은 소수점이 버려짐
+      // 실수 나눗셈은 소수까지 출력
+    
+    // 비교연산 (결과는 true/false로 나옴)
+    // == 같다
+    // != 다르다
+    // > 크다
+    // < 작다
+    // >= 크거나 같다
+    // <= 작거나 같다
+
+    // 논리 연산자
+    // && AND ==> 둘 다 true일 때 true
+    // || OR ==> 하나만 true여도 true
+    // ! NOT  ==> 결과 반전
+  }
+}
