a07c5e27c468132e325eae8c589c44f537686e84
wonjung0305 <wonjung1919@gmail.com>
2025-05-26 14:06:06 +0900

    HW8

---
+++ HW8/main.cpp
@@ -0,0 +1,15 @@
+/*
+Implement an interactive BST(Binary Search Tree)
+
+user input -> tree = insert/ delete/ print/ end
+
+input format command key value
+command = 'a' , 'd', 'p' 'q' (add, delete, print, quit)
+
+==> p , q => no key, value
+
+inoder traversal (left -> data -> right)
+
+add => if the key is already exist, printout "(key) is already in the tree"
+delete => if the key is not exist, printout "<key> is not in the tree"
+*/
