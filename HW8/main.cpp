11e55d34b960d6fe53919709643e1512fb2b8b99
wonjung0305 <wonjung1919@gmail.com>
2025-06-01 10:48:48 +0900

    DataStructure HW8

--- HW8/main.cpp
+++ HW8/main.cpp
@@ -13,3 +13,50 @@ inoder traversal (left -> data -> right)
 add => if the key is already exist, printout "(key) is already in the tree"
 delete => if the key is not exist, printout "<key> is not in the tree"
 */
+#include <iostream>
+#include <string>
+
+#include "BST.h"
+
+using namespace std;
+
+int main(){
+  BST manager;
+
+
+  while(true){  // enter the character
+      char command = ' ';
+      int userKey = 0;
+      string input = "";
+
+
+      cin >> command;
+
+      switch(command){
+        case 'a':
+          cin >> userKey; // enter the key
+          cin.ignore(); // delete buffer
+
+          getline(cin, input);
+
+          manager.addNode(userKey, input); // add node
+          break;
+        case 'd':
+          cin >> userKey; // enter the key
+          cin.ignore(); // delete buffer
+
+          manager.deleteNode(userKey);  // delete the node that user input (key)
+          break;
+        case 'p': 
+          manager.printNode();  // print all node
+          break;
+        case 'q':
+          manager.quit();
+          break;
+      }
+
+  }
+
+
+  return 0;
+}
